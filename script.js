b205c9fa8a60417081d9d83312d6df379b86895c
Vannent <erobced@gmail.com>
2022-03-09 17:10:20 -0600

    feat: link buttons to js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+const addBook = document.getElementsById("add-button");
+const markRead = document.getElementById("read-button");
+const markUnread = document.getElementById("unread-button");
+const removeBook = document.getElementById("delete-button");
+
+
 let myLibrary = []
 
 function Book() {

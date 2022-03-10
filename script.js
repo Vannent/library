fc07ac051098a22de963e3f56f455c72e7a7d309
Vannent <erobced@gmail.com>
2022-03-10 08:52:55 -0600

    complete popup function

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ const addBook = document.getElementById("add-button");
 const markRead = document.getElementById("read-button");
 const markUnread = document.getElementById("unread-button");
 const removeBook = document.getElementById("delete-button");
+const cancelButton = document.getElementById("cancel-button")
 
 addBook.addEventListener("click", function() {
     openForm();
 })
 
+cancelButton.addEventListener("click", function() {
+    closeForm()
+})
+
 
 function openForm() {
     document.querySelector(".form-popup").style.display = "flex";
+    document.querySelector(".cover").style.display = "flex";
 }
 
 function closeForm() {
-    document.getElementByClassName("form-popup").style.display = "none";
+    document.querySelector(".form-popup").style.display = "none";
+    document.querySelector(".cover").style.display = "none";
 }
 
 let myLibrary = []

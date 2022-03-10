25b09eff80b308a44c70d049bb240922e1437d55
Vannent <erobced@gmail.com>
2022-03-09 22:57:44 -0600

    feat: add button functionality for popup

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-
-const addBook = document.getElementsById("add-button");
+const addBook = document.getElementById("add-button");
 const markRead = document.getElementById("read-button");
 const markUnread = document.getElementById("unread-button");
 const removeBook = document.getElementById("delete-button");
 
+addBook.addEventListener("click", function() {
+    openForm();
+})
+
+
+function openForm() {
+    document.querySelector(".form-popup").style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementByClassName("form-popup").style.display = "none";
+}
 
 let myLibrary = []
 

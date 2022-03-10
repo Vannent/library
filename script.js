4ccf4b0c01c400130520a0eee1d76f48ca7e766f
Vannent <erobced@gmail.com>
2022-03-10 12:53:10 -0600

    feat: add full functionality to add book

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const addBook = document.getElementById("add-button");
 const markRead = document.getElementById("read-button");
 const markUnread = document.getElementById("unread-button");
 const removeBook = document.getElementById("delete-button");
-const cancelButton = document.getElementById("cancel-button")
+const cancelButton = document.getElementById("cancel-button");
+const submitButton = document.getElementById("submit-button");
 
 addBook.addEventListener("click", (e) => openForm())
 cancelButton.addEventListener("click", (e) => closeForm())
-markRead.addEventListener("click", (e) => readBook())
-markUnread.addEventListener("click", (e) => unreadBook())
-removeBook.addEventListener("click", (e) => deleteBook())
+submitButton.addEventListener("click", (e) => addBookToLibrary());
 
 
 function openForm() {
@@ -21,25 +20,74 @@ function closeForm() {
     document.querySelector(".cover").style.display = "none";
 }
 
-function readBook() {
-    document.querySelector(".book-card").style.backgroundColor = "#75EDBF";
-}
-
-function unreadBook() {
-    document.querySelector(".book-card").style.backgroundColor = "#f7f7f7a4";
-}
-
-function deleteBook() {
-
-}
-
-let myLibrary = []
-
 function Book(name, author) {
     this.name = name
     this.author = author
 }
 
+// Display Book
+let myLibrary = [
+    {
+        name: "Lord of the Rings",
+        author: "J. R. R. Tolkien"
+    }
+];
+
+const books = myLibrary
+books.forEach((book) => addBooktoList(book));
+
+function addBooktoList(book) {
+    const list = document.querySelector(".book-cards");
+    const bookCard = document.createElement("div");
+    bookCard.className = "book-card"
+
+    bookCard.innerHTML = `
+    <div class="book-title">${book.name}</div>
+    <div class="author">${book.author}</div>
+    <div class="book-buttons">
+        <button><img src="./img/check.png" class="read-button"></button>
+        <button><img src="./img/unread.png" class="unread-button"></button>
+        <button><img src="./img/delete.png" class="delete-button"></button>
+    </div>
+    `;
+    list.appendChild(bookCard);
+};
+
+function clearField() {
+    const name = document.querySelector("#book_name").value = "";
+    const author = document.querySelector("#author_name").value = "";
+};
+
+document.addEventListener("DOMContentLoaded", UIEvent.displayBooks);
+
+// Add Book to Library
 function addBookToLibrary() {
-    
-}
+    const name = document.querySelector("#book_name").value;
+    const author = document.querySelector("#author_name").value;
+    const book = new Book(name, author);
+
+    if(name === "" || author === "") alert("Please fill the fields.")
+    else {
+        console.log(book);
+        addBooktoList(book); // add book to UI.
+        closeForm();
+        clearField();
+    }
+};
+
+document.querySelector(".book-cards").addEventListener("click", (e) => {
+    manageBook(e.target);
+});
+
+// Remove Book from Library
+function manageBook(target) {
+    if(target.classList.contains("delete-button")) {
+        target.parentElement.parentElement.parentElement.remove();
+    };
+    if(target.classList.contains("read-button")) {
+        target.parentElement.parentElement.parentElement.style.backgroundColor = "#75EDBF";
+    };
+    if(target.classList.contains("unread-button")) {
+        target.parentElement.parentElement.parentElement.style.backgroundColor = "#f7f7f7a4";
+    };
+};
